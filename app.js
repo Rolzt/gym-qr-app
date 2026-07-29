da7b9e4231fbab0d5ca176ad07ce594a665be37e
Rolzt <Rolst22@gmail.com>
2026-07-29 08:59:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -826,7 +826,7 @@ exerciseSelect.value = exerciseKey;
 
 exerciseSelect.addEventListener("change", function () {
 
-    window.location.href = `gym.html?exercise=${this.value}`;
+    window.location.search = `?exercise=${this.value}`;
 
 });
 
